b277b8cc3ea16c2dce2e71c9ccdcd11da700b5b7
John Romp Un <romp@rompdit.com>
2025-05-22 17:56:17 -0500

    rust library

--- src/c4/mod.rs
+++
@@ -1,1 +0,0 @@
-pub(crate) mod c4_file;
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-mod c4;
-
+use c4id::c4::c4_file;
 fn main() {
 
     let args: Vec<String> = std::env::args().collect();
     if let Some(filename) = args.get(1) {
-        let result = c4::c4_file::c4_file(filename);
+        let result = c4_file(filename);
         println!("{}", result);
     } else {
         eprintln!("Please provide a filename as the first argument.");
